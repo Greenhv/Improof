5fa537ac7fcda9daf3ba0c7a9f8f572be996a6ec
Herbert V <herbert.videla@tektonlabs.com>
2016-11-14 00:55:54 -0500

    Project create modal

--- components/MaterialSelect.jsx
+++ components/MaterialSelect.jsx
@@ -4,12 +4,13 @@ import { Icon } from 'react-onsenui';
 export default class MaterialSelect extends React.Component {
   render() {
     const {
-      data
+      data,
+      onChange,
     } = this.props;
 
     return (
       <div className="material-select-container">
-        <select className="material-select" name="material-select" >
+        <select onChange={onChange} className="material-select" name="material-select" >
           <option value="" disabled></option>
           {data.map((val) => {
             return(<option value={val.id} key={val.id}>{val.value}</option>)
--- containers/MyProjects.jsx
+++ containers/MyProjects.jsx
@@ -2,13 +2,39 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
-import { Page, Button } from 'react-onsenui';
+import { Page, Button, Dialog, Input } from 'react-onsenui';
 import NavBar from '../components/NavBar.jsx';
 import ProjectList from '../components/ProjectList.jsx';
+import MaterialSelect from '../components/MaterialSelect.jsx';
 
 import * as Actions from '../actions';
 
 class MyProjects extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dialogShown: false,
+      projectCategory: "",
+      projectName: "",
+    };
+  }
+
+  handleInputOnChange(type,e) {
+      this.setState({[type]: e.target.value});
+  }
+  showDialog() {
+    this.setState({dialogShown: true});
+  }
+
+  hideDialog() {
+    this.setState({dialogShown: false});
+  }
+
+  createNewProject() {
+    //Algo
+    this.hideDialog();
+  }
+
   render() {
     const {
       navigator,
@@ -29,8 +55,39 @@ class MyProjects extends React.Component {
       >
         <ProjectList currentUser={currentUser} />
         <div className="text-center">
-          <Button name="create-project"  className="dark-btn pomodoro-button">Start new Project</Button>
+          <Button name="create-project" onClick={() => {this.showDialog()}} className="dark-btn pomodoro-button">Start new Project</Button>
         </div>
+        <Dialog
+         isOpen={this.state.dialogShown}
+         isCancelable={true}
+         onCancel={() => {this.hideDialog()}}
+        >
+          <div className="dialog-content">
+            <div className="dialog-header text-center">
+              Start a new Project
+            </div>
+            <div className="dialog-body">
+              <div className="flex-wrapper flex-absolute-center">
+                <div className="dialog-body-icon">
+                  <img className="user-projects-img" src="https://dummyimage.com/90x90/9b9b9b/000000" alt="Project Type Icon"/>
+                </div>
+                <div className="dialog-body-data">
+                  <Input
+                   className="input-full-width dialog-body-input"
+                   placeholder="Name"
+                   type="text"
+                   onChange={(e) => this.handleInputOnChange('projectName', e)}
+                  />
+                  <MaterialSelect data={[{id: 1, value: 'Test Option'}, {id: 2, value: 'Test Option 2'}]} onChange={(e) => this.handleInputOnChange('projectCategory', e)}/>
+                </div>
+              </div>
+              <div className="flex-wrapper flex-absolute-center">
+                <Button className="light-btn short-btn dialog-content-btn" onClick={() => {this.hideDialog()}}>Cancel</Button>
+                <Button className="dark-btn short-btn" onClick={() => {this.createNewProject()}}>Accept</Button>
+              </div>
+            </div>
+          </div>
+        </Dialog>
       </Page>
     );
   }
